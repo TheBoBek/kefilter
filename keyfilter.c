8355d97d2e49396546fd40b6fce2546302413a08
TheBoBek <85736591+TheBoBek@users.noreply.github.com>
2023-10-28 20:52:02 +0200

    Add files via upload

--- keyfilter.c
+++ keyfilter.c
@@ -2,171 +2,165 @@
 #include <ctype.h>
 #include <string.h>
 
-void str_upper(char *strng, char *vysledok){ //zadany retazec na uppercase
+#define LINE_LENGHT 101
+
+// Function combines arguments of the input to one string
+void strCombiner(int sumArguments, char **arguments, char *search){
+    int i=1;
+    int argLenght; // String argLenght of single argument
+        strcpy(search, arguments[i]);
+        while (i < sumArguments - 1){
+            argLenght = strlen(search);
+            // Append next argument to search string
+            search[argLenght]= ' ';
+            search[argLenght + 1] = '\0';
+            strcat(search, arguments[i + 1]);
+
+            i++;
+        }
+}
+// Function changes the input string to uppercase
+void str_upper(char *strng, char *vysledok){ 
     int dlzka = strlen(strng);
 
     for (int i = 0; i < dlzka; i++) {
         vysledok[i] = toupper(strng[i]);
     }
     vysledok[dlzka] = '\0';
-    // printf("%s", vysledok);
 }
-int line_reader(char *pismenka, char *vyhlad, char *posledny_reatzec){ // Porovnavanie charkterov
-    char pismenko;
-    char line[101];
+// Function iterates through chars of the line
+int line_reader(char *enabledChars, char *vyhlad, char *lastInputLine){
+    char line[LINE_LENGHT];
     int i = 0;
     int dlzka_vyhlad = strlen(vyhlad);
-    int flag = 1;
-    int flag2 = 1;
-    // printf("%s-", vyhlad);
-    while (1)
+    int flag = 1; // Check for EOF
+    int areSame = 1; // Check for same strings
+
+    line[i] = toupper(getchar());
+    while (line[i] != '\n')
     {   
-        // printf("Bug2");
-
-        line[i] = toupper(getchar());
-
         if (i < dlzka_vyhlad){
             if(line[i] != toupper(vyhlad[i])){ // Porovnava i-ty char riadku s i-tym charom vyhladania
-                    flag2= 0;
+                    areSame= 0;
             }
         }
-        
-            
-
         if (line[i] == EOF){
-            line[i] = '\0';
             flag = 0;
             break;
         }
-
-        else if(line[i] == '\n'){
-            line[i] = '\0';
-            break;
-        }
         i++;
+        line[i] = toupper(getchar());
+
     }
-    // printf("Interacia sa rovna: %d\n", i);
-    if (flag2 == 1){ // Ak sa zhoduje riadok s vyhladanim, appenduje sa nasledujuce pismenko riadka do listu pvl znakov
-        if (i == dlzka_vyhlad && dlzka_vyhlad != 0){
-            printf("Found: %s\n", line);
+    line[i] = '\0';
+    // If searched string correspond with line
+    // Ak sa zhoduje riadok s vyhladanim, appenduje sa nasledujuce pismenko riadka do listu pvl znakov
+    if (areSame == 1){ 
+        if ((i == dlzka_vyhlad) && (dlzka_vyhlad != 0)){
+            strcpy(lastInputLine, line);
             flag = -1;
         }
         else{
-            pismenko = line[dlzka_vyhlad];
-            int lenght = strlen(pismenka);
+            char pismenko;
 
-            pismenka[lenght] = pismenko;
-            pismenka[lenght + 1] = '\0';
-            strcpy(posledny_reatzec, line);
+            pismenko = line[dlzka_vyhlad];
+            int argLenght = strlen(enabledChars);
+
+            enabledChars[argLenght] = pismenko;
+            enabledChars[argLenght + 1] = '\0';
+            strcpy(lastInputLine, line);
         }
     }
     return flag;
 }
-
-void zorad(char *pismenka, int n){
-    // printf("NEzoradene pismenka:%s\n", pismenka);
-
-    int i, j, min_index, temp, tail;
-    tail = 1;
+// Function sorts string of enabled chars alphabeticaly
+void sortAlpabeticaly(char *enabledChars, int n){
+    int i, j, min_index, temp;
     for (i = 0; i < n; i++) {
         // Find the minimum element in the unsorted part of the array
         min_index = i;
         for (j = i + 1; j < n; j++) {
-            if (pismenka[j] < pismenka[min_index]) {
+            if (enabledChars[j] < enabledChars[min_index]) {
                 min_index = j;
             }
         }
-
-        // Swap the found minimum element with the element at index i
-        temp = pismenka[i];
-        pismenka[i] = pismenka[min_index];
-        pismenka[min_index] = temp;
+        // Change minimum element with element at index i
+        temp = enabledChars[i];
+        enabledChars[i] = enabledChars[min_index];
+        enabledChars[min_index] = temp;
     }
-    // printf("Zoradene pismenka:%s\n", pismenka);
-
+}
+// Function removes duplicates by editing string of enabled chars
+void removeDuplicates(char *enabledChars, int n){
+    int temp = 1;
     for (int i = 1; i < n + 1; i++) {
         int j;
-        for (j = 0; j < tail; j++) {
-            if (pismenka[i] == pismenka[j]) {
+        for (j = 0; j < temp; j++) {
+            if (enabledChars[i] == enabledChars[j]) {
                 break; // Character is already encountered, it's a duplicate
             }
         }
-        if (j == tail) {
-            pismenka[tail] = pismenka[i];
-            tail++;
+        if (j == temp) {
+            enabledChars[temp] = enabledChars[i];
+            temp++;
         }
     }
-    pismenka[tail] = '\0';
-    // printf("Dlzka pismenok: %s\n", pismenka);
-    // printf("PISMENKA SU: %s", pismenka);
-
-    if (pismenka[0] == '\0'){
-        printf("Not found\n");
-    }
-    else if (n > 1){
-        // printf("Zoradene pismenka, odstranene duplikaty:'%s'\n", pismenka);
-        printf("Enable: %s\n", pismenka);
-    }
+    enabledChars[temp] = '\0';
 }
-
-int main(int argc, char *argv[]){
-    char posledny_retazec[101];
-    char pvl_znaky[27];
-    int shd_continue = 1;
-    char retazec[101]; // Vyhladavanie
-
-    int i;
-    str_upper(retazec, retazec); //zadany retazec na uppercase
-    // printf("%d", argc);
-
-    if (argc == 1){
-        retazec[0] = '\0';
-    }
-    else if (argc == 2){
-        strcpy(retazec, argv[1]);
-    }
-    else if (argc > 2){
-        // printf("Bug\n");
-        
-        
-        i=1;
-        int lenght;
-        strcpy(retazec, argv[i]);
-        while (i < argc - 1){
-            lenght = strlen(retazec);
-            // printf("%d\n", lenght);
-            retazec[lenght]= ' ';
-            retazec[lenght + 1] = '\0';
-            strcat(retazec, argv[i + 1]);
-            // printf("Argument %d sa rovná '%s'\n", i, retazec);
-
-            i++;
-        }
-        
-
-    }
-    i = 0;
-    while (shd_continue == 1){
-        shd_continue = line_reader(pvl_znaky,retazec, posledny_retazec);
-        // printf("Posledny retazec je: %s\n", posledny_retazec);
-        // printf("%d, %s\n", i, posledny_retazec);
-        // printf("Iteracia:%d\n", i);
-        // printf("Pismenka su: %s\n", pvl_znaky);
-        i++;
-    }
-    
-    int n = strlen(pvl_znaky);
+// Function sorts string of enabled chars
+void sort(char *enabledChars, int n){
+    sortAlpabeticaly(enabledChars, n);
+    removeDuplicates(enabledChars, n);
+}
+// Function prints results of the program
+void resultPrinter(char *enabledChars, char *lastInputLine, int shouldContinue){
+    int n = strlen(enabledChars);
     if (n == 1){
-        printf("Found: %s\n", posledny_retazec);
+        printf("Found: %s\n", lastInputLine);
         }
     else {
-        // printf("Cely string je: %s a pocet znakov je: %d\n", pvl_znaky, n);
-        if (shd_continue != -1){
-            zorad(pvl_znaky, n);
+        if (shouldContinue != -1){
+            sort(enabledChars, n);
+            if (enabledChars[0] == '\0'){
+                printf("Not found\n");
+            }
+            else if (n > 1){
+                printf("Enable: %s\n", enabledChars);
+            }
         }
+        else if (shouldContinue == -1){
+            printf("Found: %s\n", lastInputLine);
+        }
+    }
+}
+int main(int argc, char *argv[]){
+    char search[LINE_LENGHT]; // Searched string prefix, entered by user
+    char lastInputLine[LINE_LENGHT]; // Last saved line from the stdin
+    char enabledChars[LINE_LENGHT]; // Enabled characters TODO: Make program that will work for unlimited duplicated chars
 
+    // Converts all characters from string to upper case 
+    str_upper(search, search); 
+
+    // Initializes empty search string
+    if (argc == 1){ 
+        search[0] = '\0';
+    }
+    else if (argc == 2){
+        strcpy(search, argv[1]);
     }
 
+    // Initializes search string with multiple words separated by 'space'
+    else if (argc > 2){ 
+        strCombiner(argc, argv, search);
+    }
+
+    // Loops through input data(stdin)
+    int shouldContinue = 1;
+    while (shouldContinue == 1){
+        shouldContinue = line_reader(enabledChars, search, lastInputLine);
+    }
+    
+    resultPrinter(enabledChars, lastInputLine, shouldContinue);
 
     return 0;
 }
